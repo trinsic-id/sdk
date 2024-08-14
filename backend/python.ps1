3699c710923db8bf24c08e9187c3c05aea15ad78
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 14:38:36 -0700

    Build and validate Python (#5)

--- backend/python.ps1
+++ backend/python.ps1
@@ -1,7 +1,33 @@
 $additionalProperties = @{
-    packageName    = "trinsic_connect_backend_client"
+    packageName    = "trinsic_connect"
     packageVersion = "[VERSION]"
     packageUrl     = "https://trinsic.id"
-    projectName    = "Trinsic-Connect-Backend-Client"
+    projectName    = "Trinsic-Connect"
 }
 & "$PSScriptRoot/generate-client.ps1" -language "python" -patchVersion "1" -additionalProperties $additionalProperties
+
+try {
+    # Node doesn't like building from different directories but I don't want to change location all the time so pop at end back to whatever started the script
+    Push-Location "$PSScriptRoot/../dist/python"
+    # Check if 'python' is available
+    $pythonPath = (Get-Command python -ErrorAction SilentlyContinue).Path
+
+    # If 'python' is not available, check for 'python3'
+    if (-not $pythonPath) {
+        $pythonPath = (Get-Command python3 -ErrorAction SilentlyContinue).Path
+    }
+
+    # If neither 'python' nor 'python3' is available, throw an error
+    if (-not $pythonPath) {
+        Write-Error "Neither 'python' nor 'python3' was found in your system path."
+        exit 1
+    }
+
+    # Execute the Python script using the found interpreter
+    & $pythonPath setup.py sdist --dist-dir "$PSScriptRoot/../dist/publish" bdist_wheel --dist-dir "$PSScriptRoot/../dist/publish"
+    & pip install .
+}
+finally {
+    Pop-Location
+}
+
