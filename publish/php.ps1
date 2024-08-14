34f1c134b03b75604aa8740de0233040ebee2cd3
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 14:19:37 -0700

    Add PHP generation (#4)

--- publish/php.ps1
+++ publish/php.ps1
@@ -1,11 +1,12 @@
-$sourceLocation = "$PSScriptRoot/../dist/go"
+$sourceLocation = "$PSScriptRoot/../dist/php"
 
 if (-not (Test-Path -Path $sourceLocation -PathType Container)) {
     throw "The source location '$sourceLocation' does not exist."
 }
 
-#We likely need to split php up into a separate repo. Because of that we should do the same for go
+#We likely need to split php up into a separate repo as with go.
 throw "Not implemented yet"
-$tagName = "go/v$env::VERSION"
+
+$tagName = "php/v$env::VERSION"
 git tag $tagName
 git push origin $tagName
