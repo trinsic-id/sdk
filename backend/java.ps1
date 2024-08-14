5278d8b058aafcd0e9a56b2a0a805d7580cdb651
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 13:32:41 -0700

    Works?

--- backend/java.ps1
+++ backend/java.ps1
@@ -5,9 +5,9 @@ $additionalProperties = @{
     modelPackage             = "id.trinsic.connect.models"
     artifactId               = "connect"
     groupId                  = "id.trinsic"
-    scmConnection            = ""
-    scmDeveloperConnection   = ""
-    scmUrl                   = "https://github.com/trinsic-id"
+    scmConnection            = "scm:git:https://github.com/trinsic-id/connect-sdks.git"
+    scmDeveloperConnection   = "scm:git:ssh://git@github.com/trinsic-id/connect-sdks.git"
+    scmUrl                   = "https://github.com/trinsic-id/connect-sdks"
     artifactUrl              = "https://trinsic.id"
     developerEmail           = "support@Trinsic.id"
     developerName            = "Trinsic"
@@ -17,10 +17,16 @@ $additionalProperties = @{
 }
 & "$PSScriptRoot/generate-client.ps1" -language "java" -patchVersion "1" -additionalProperties $additionalProperties
 
-
 try {
     # Java doesn't like building from different directories but I don't want to change location all the time so pop at end back to whatever started the script
     Push-Location "$PSScriptRoot/../dist/java"
+
+    # OpenAPI generator doesn't generate archiveClassifier but we use gradle 8+ which requires it
+    $buildGradleFile = "build.gradle"
+    $buildGradleFileContent = Get-Content -Path $buildGradleFile
+    $buildGradleFileContent = $buildGradleFileContent -replace "classifier =", "archiveClassifier="
+    $buildGradleFileContent | Set-Content -Path $buildGradleFile
+
     & gradle compileJava
     & gradle jar
 }
