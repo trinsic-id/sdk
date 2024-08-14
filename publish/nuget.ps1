062f959b9b2d473926b0c598b99a82a46f1f259b
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 09:46:04 -0700

    Prrr

--- publish/nuget.ps1
+++ publish/nuget.ps1
@@ -1,6 +1,4 @@
-$sourceLocation = "../dist/csharp"
-#Ensure we run this "from this folder" as we run it personally in a command line as well as through the perspective of builds.
-Set-Location -Path "$PSScriptRoot"
+$sourceLocation = "$PSScriptRoot/../dist/csharp"
 
 if (-not (Test-Path -Path $sourceLocation -PathType Container)) {
     throw "The source location '$sourceLocation' does not exist."
