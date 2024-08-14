31c1d7f5fb1e69c8e9fb31041cac460652fb6f8c
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 10:38:28 -0700

    Move go "prep build" logic

--- publish/go.ps1
+++ publish/go.ps1
@@ -4,11 +4,6 @@ if (-not (Test-Path -Path $sourceLocation -PathType Container)) {
     throw "The source location '$sourceLocation' does not exist."
 }
 
-$goModFile = "$sourceLocation/go.mod"
-$goModFileContent = Get-Content -Path $goModFile
-$goModFileContent = $goModFileContent -replace "/GIT_USER_ID/GIT_REPO_ID", "/trinsic-id/connect-sdks/dist/go"
-$goModFileContent | Set-Content -Path $goModFile
-
 $tagName = "go/v$env::VERSION"
 git tag $tagName
 git push origin $tagName
