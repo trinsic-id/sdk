6521160e368dadb75b310f7807e99da88d60bd52
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 10:51:47 -0700

    Java

--- server/java.ps1
+++ server/java.ps1
@@ -1,9 +1,9 @@
 $additionalProperties = @{
-    apiPackage               = "id.trinsic.connect.backendclient"
+    apiPackage               = "id.trinsic.connect"
     artifactVersion          = "[VERSION]"
     library                  = "native"
-    modelPackage             = "id.trinsic.connect.backendclient.models"
-    artifactId               = "connect-backendclient"
+    modelPackage             = "id.trinsic.connect.models"
+    artifactId               = "connect"
     groupId                  = "id.trinsic"
     scmConnection            = ""
     scmDeveloperConnection   = ""
@@ -13,6 +13,18 @@ $additionalProperties = @{
     developerName            = "Trinsic"
     developerOrganization    = "Trinsic"
     developerOrganizationUrl = "https://trinsic.id"
-    artifactDescription      = "'Trinsic Connect Backend Client'"
+    artifactDescription      = "'Trinsic Connect'"
 }
 & "$PSScriptRoot/generate-client.ps1" -language "java" -patchVersion "1" -additionalProperties $additionalProperties
+
+
+try {
+    # Java doesn't like building from different directories but I don't want to change location all the time so pop at end back to whatever started the script
+    Push-Location "$PSScriptRoot/../dist/java"
+    & gradle compileJava
+    & gradle jar
+}
+finally {
+    Pop-Location
+}
+
