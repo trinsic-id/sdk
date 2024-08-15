4740ed3f6e0c8559638772aff16d93c9e2ea7ae8
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-15 16:26:16 -0700

    Fix the versions

--- get-version.ps1
+++ get-version.ps1
@@ -1,12 +1,13 @@
 # Parameters:
 param (
     [Parameter(Mandatory = $true)]
-    [string]$patchVersion
+    [string]$versionName
 )
 # Because there's a lot of small variations in version number usage across generators, generate version first and provide it via additonal properties to the generate-client.ps1 file.
 # Other option was a lot of if language specific if statements inside of generate-client.
 $jsonContent = Get-Content -Path "$PSScriptRoot\versions.json" -Raw | ConvertFrom-Json
 $backendVersion = $jsonContent.backendVersion
+$patchVersion = $jsonContent."$($versionName)patchVersion"
 $packageVersion = "$backendVersion.$patchVersion"
 Write-Host "Package will be created with version $packageVersion from file based API Version $version and patchVersion $patchVersion"
 
