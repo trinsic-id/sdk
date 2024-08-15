c8ec5b4212bb51af1ff58bd8280125ea8a23e8a6
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 20:35:07 -0700

    Add specific symbols output

--- backend/csharp.ps1
+++ backend/csharp.ps1
@@ -7,13 +7,13 @@ $additionalProperties = @{
     useDateTimeOffset      = "true"
     validatable            = "false"
 }
-& "$PSScriptRoot/generate-client.ps1" -language "csharp" -patchVersion "0" -additionalProperties $additionalProperties
+& "$PSScriptRoot/generate-client.ps1" -language "csharp" -patchVersion "1" -additionalProperties $additionalProperties
 
 try {
     Push-Location "$PSScriptRoot/../dist/csharp"
     & dotnet restore
     & dotnet build --configuration Release --no-restore
-    & dotnet pack --configuration Release --no-build --no-restore --include-source --include-symbols --output "$PSScriptRoot/../dist/publish"
+    & dotnet pack --configuration Release --no-build --no-restore --include-source --include-symbols -p:SymbolPackageFormat=snupkg --output "$PSScriptRoot/../dist/publish"
 }
 finally {
     Pop-Location
