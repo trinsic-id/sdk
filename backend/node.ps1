d7d1f46cc9e7aacbd5c3d29b8926e5ffc0e04100
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-15 09:21:25 -0700

    Publish to NPM (#9)

--- backend/node.ps1
+++ backend/node.ps1
@@ -1,15 +1,13 @@
 $additionalProperties = @{
-    npmName       = "@trinsic/connect-node"
-    npmVersion    = "[VERSION]"
-    supportsES6   = "true"
-    npmRepository = "https://github.com/trinsic-id"
+    npmName     = "@trinsic/connect-node"
+    npmVersion  = "[VERSION]"
+    supportsES6 = "true"
 }
 & "$PSScriptRoot/generate-client.ps1" -language "typescript-fetch" -patchVersion "0" -additionalProperties $additionalProperties -outputFolder "$PSScriptRoot/../dist/node"
 
 try {
     Push-Location "$PSScriptRoot/../dist/node"
     & npm install
-    & npm run build
     & npm pack --pack-destination "$PSScriptRoot/../dist/publish"
 }
 finally {
