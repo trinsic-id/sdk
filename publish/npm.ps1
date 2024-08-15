2cf4621fee4a2008f039cc63cefd048148f888f4
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-14 21:04:54 -0700

    Publish to NPM

---
+++ publish/npm.ps1
@@ -0,0 +1,16 @@
+Write-Host "Setting npm token"
+npm set //registry.npmjs.org/:_authToken $env.NPM_TOKEN
+
+try {
+    Set-Location "$PSScriptRoot/../dist/publish"
+    Write-Host "Publishing packages"
+    foreach ($package in Get-ChildItem -Filter *.tgz) {
+        Write-Host "Publishing $package.FullName"
+        npm publish $package.FullName  --yes --no-git-tag-version
+        Write-Host "Published $package.FullName"
+    }
+}
+finally {
+    Pop-Location
+}
+
