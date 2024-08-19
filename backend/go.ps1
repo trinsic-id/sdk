7ce66aafbf632e8dda6a2ca6de3b4de304ec5540
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-19 16:24:51 -0700

    Specify correct go repo path

--- backend/go.ps1
+++ backend/go.ps1
@@ -7,7 +7,7 @@ try {
     Push-Location "$PSScriptRoot/../dist/go"
     $goModFile = "go.mod"
     $goModFileContent = Get-Content -Path $goModFile
-    $goModFileContent = $goModFileContent -replace "/GIT_USER_ID/GIT_REPO_ID", "/trinsic-id/connect-sdks/dist/go"
+    $goModFileContent = $goModFileContent -replace "/GIT_USER_ID/GIT_REPO_ID", "/trinsic-id/connect-go"
     $goModFileContent | Set-Content -Path $goModFile
 
     &go build
