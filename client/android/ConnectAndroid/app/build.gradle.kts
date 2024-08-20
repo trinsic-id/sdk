05c0653b5a6065635a631678a9de53f1e46af1c0
Joshua Coffey <josh@trinsic.id>
2024-08-20 10:09:13 -0400

    Update Android version

--- client/android/ConnectAndroid/app/build.gradle.kts
+++ client/android/ConnectAndroid/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
-    implementation(project(":ConnectAndroid"))
+    implementation("id.trinsic.connect-android")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
