ca662490699a72733e593b3cd094ce2329b06a6a
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-20 11:48:58 -0700

    Secrets

--- publish/rubygems.ps1
+++ publish/rubygems.ps1
@@ -1,6 +1,6 @@
 Write-Host "Publishing RubyGems package..."
 
-$env:GEM_HOST_API_KEY = $env:RUBYGEMS_API_KEY
+$env:GEM_HOST_API_KEY = $env:RUBYGEMS_API_TOKEN
 &gem signin  1> $null
 
 &gem push dist/publish/*.gem
