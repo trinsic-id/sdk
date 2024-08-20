bf31325ec5f8853e46a6f9035fa10117a3cbe0f3
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-20 11:41:34 -0700

    Test things

--- publish/pypi.ps1
+++ publish/pypi.ps1
@@ -3,7 +3,7 @@ Write-Host "Publishing to PyPi"
 $env:TWINE_USERNAME = "__token__"
 $env:TWINE_PASSWORD = "$env:PYPI_API_TOKEN"
 
-&pip install twine
+&pip install twine 1> $null
 
 &twine upload dist/publish/*
 
