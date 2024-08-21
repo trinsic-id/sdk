ecfa1e0aceafdacf138eeaae22ddfb6e437112a7
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-21 11:00:26 -0700

    Up versions to 0.3.0

--- publish/go.ps1
+++ publish/go.ps1
@@ -1,6 +1,6 @@
 $sourceLocation = "$PSScriptRoot/../dist/go"
 $destinationLocation = "$PSScriptRoot/../go"
-$repositoryPath = "trinsic-id/connect-go"
+$repositoryPath = "trinsic-id/sdk-go-api"
 $name = "Go"
 
 & "$PSScriptRoot\submodule.ps1" -sourceLocation $sourceLocation -name $name -destinationLocation $destinationLocation -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
--- publish/php.ps1
+++ publish/php.ps1
@@ -1,6 +1,6 @@
 $sourceLocation = "$PSScriptRoot/../dist/php"
 $destinationLocation = "$PSScriptRoot/../php"
-$repositoryPath = "trinsic-id/connect-php"
+$repositoryPath = "trinsic-id/sdk-php-api"
 $name = "PHP"
 
 & "$PSScriptRoot\submodule.ps1" -sourceLocation $sourceLocation -name $name -destinationLocation $destinationLocation -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
