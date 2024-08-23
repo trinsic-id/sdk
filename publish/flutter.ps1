39353e49a6933d8a8809ed12cf1712d6ed5b5d6d
Joshua Coffey <josh@trinsic.id>
2024-08-23 12:51:09 -0400

    Add flutter action

---
+++ publish/flutter.ps1
@@ -0,0 +1,22 @@
+param(
+    [switch]$dryRun
+)
+
+try {
+    Set-Location "$PSScriptRoot/../client/flutter/trinsic_flutter_ui"
+    # Write-Host "Setting version to $version"
+
+    # $fileContent = Get-Content -Path "pubspe c.yaml"
+    # $fileContent = $fileContent -replace "VERSION_REPLACEME", "$version"
+    # $fileContent | Set-Content -Path "pubspec.yaml"
+
+    Write-Host "Publishing package"
+    if($dryRun) {
+        flutter pub publish --dry-run
+    } else {
+        flutter pub publish
+    }
+}
+finally {
+    Pop-Location
+}
