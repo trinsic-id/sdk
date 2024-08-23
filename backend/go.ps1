61a93e7b474c96b718d2e33e2a439a8732a43910
Lucas Castro <53352488+lucasamonrc@users.noreply.github.com>
2024-08-23 10:24:45 -0500

    Update go.ps1

--- backend/go.ps1
+++ backend/go.ps1
@@ -1,7 +1,7 @@
 $additionalProperties = @{
     packageName    = "trinsic_api"
     packageVersion = "[VERSION]"
-    disallowAdditionalPropertiesIfNotPresent = "true"
+    disallowAdditionalPropertiesIfNotPresent = "false"
 }
 & "$PSScriptRoot\generate-client.ps1" -language "go" -additionalProperties $additionalProperties
 try {
