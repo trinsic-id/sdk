01e89b2117df42a8b00849709c07771787a49562
Lucas Castro <53352488+lucasamonrc@users.noreply.github.com>
2024-08-23 10:12:06 -0500

    fix: Allow additional properties in go (#41)

--- backend/go.ps1
+++ backend/go.ps1
@@ -1,6 +1,7 @@
 $additionalProperties = @{
     packageName    = "trinsic_api"
     packageVersion = "[VERSION]"
+    disallowAdditionalPropertiesIfNotPresent = "false"
 }
 & "$PSScriptRoot\generate-client.ps1" -language "go" -additionalProperties $additionalProperties
 try {
