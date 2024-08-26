11332f36d91ac7ed422b539c1dff1f88e41a473e
Joshua Coffey <josh@trinsic.id>
2024-08-26 10:24:11 -0400

    Fix tests

--- client/flutter/trinsic_flutter_ui/example/integration_test/plugin_integration_test.dart
+++ client/flutter/trinsic_flutter_ui/example/integration_test/plugin_integration_test.dart
@@ -10,7 +10,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:trinsic_flutter/trinsic_flutter.dart';
+import 'package:trinsic_flutter_ui/trinsic_flutter_ui.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
