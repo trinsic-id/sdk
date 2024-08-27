3598ff95e223fa003f9cf2663580c73c1afa8a57
Joshua Coffey <josh@trinsic.id>
2024-08-27 16:04:56 -0400

    Improve sample app

--- react-native-ui/example/src/App.tsx
+++ react-native-ui/example/src/App.tsx
@@ -1,22 +1,34 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { launchSession } from '@trinsic/react-native-ui';
+import type { LaunchSessionResult } from '../../src/ReactNativeUi';
+
+const SESSION_CREATE_URL = "[REPLACE_ME]";
 
 export default function App() {
-  const [result, setResult] = useState<string | null>();
+  const [result, setResult] = useState<LaunchSessionResult | null>();
+  const [error, setError] = useState<string | null>();
 
-  useEffect(() => {
-    launchSession('https://test.com', 'example://callback').then((r) =>
-      setResult(
-        r.resultsAccessKey +
-          (r.sessionId === null ? 'session is null' : 'session is not null')
-      )
-    );
-  }, []);
+  const handleButtonPress = async () => {
+    const getSessionUrlResult = await fetch(SESSION_CREATE_URL);
+    const launchUrl = await getSessionUrlResult.text();
+    const callbackUrl = 'trinsic-ui-example-redirect-scheme-react-native://callback';
+    const result = await launchSession(launchUrl, callbackUrl);
+    setResult(result);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Button title="Launch Session"
+        onPress={handleButtonPress}
+      />
+      {!!error && <Text>{error}</Text>}
+      {!!result && <>
+        <Text>Session ID: {result.sessionId}</Text>
+        <Text>Results Access Key: {result.resultsAccessKey}</Text>
+        <Text>Success: {result.success ? 'true' : 'false'}</Text>
+        <Text>Cancelled: {result.canceled ? 'true' : 'false'}</Text>
+      </>}
     </View>
   );
 }
