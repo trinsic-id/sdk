825e062e5f99d53d9ce82ccc2c2774b75c9b8c9b
Lucas Castro <lucasamonrc@gmail.com>
2024-08-28 17:22:18 -0500

    Ignore dist

--- ui-web/samples/dist/assets/redirect-DFHngonJ.js
+++
@@ -1,2 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const e=new URLSearchParams(window.location.search),o={success:e.get("success")==="true",resultsAccessKey:e.get("resultsAccessKey")};console.debug("Sending message to opener",o,window.opener);var s;(s=window.opener)==null||s.postMessage(o,"*");
-//# sourceMappingURL=redirect-DFHngonJ.js.map
