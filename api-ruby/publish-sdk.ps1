09675c4c4bbd6b0a5a56b923ce2c5f8a5a697d12
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-28 11:55:07 -0700

    Fix ruby publish

--- api-ruby/publish-sdk.ps1
+++ api-ruby/publish-sdk.ps1
@@ -3,4 +3,16 @@ Write-Host "Publishing RubyGems package..."
 $env:GEM_HOST_API_KEY = $env:RUBYGEMS_API_TOKEN
 &gem signin  1> $null
 
-&gem push sdk/publish/*.gem
+
+try {
+    Set-Location "$PSScriptRoot/sdk/publish"
+    Write-Host "Publishing packages"
+    foreach ($package in Get-ChildItem -Filter "trinsic_api-*.gem") {
+        Write-Host "Publishing $package"
+        &gem push $package.FullName
+        Write-Host "Published $package"
+    }
+}
+finally {
+    Pop-Location
+}
