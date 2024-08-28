632dd81d118e0c1b080c7ce8d4784a2c70876016
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-27 19:03:25 -0700

    Fix web ui

--- ui-web/build-sdk.ps1
+++ ui-web/build-sdk.ps1
@@ -1,5 +1,6 @@
 try {
     Push-Location "$PSScriptRoot\sdk"
+    
     & npm install
 
     $version = &"$PSScriptRoot\..\get-version.ps1" -versionName "web";
@@ -7,6 +8,10 @@ try {
     & npm version "$version" --no-git-tag-version
 
     & npm run build
+
+    if (-not (Test-Path -Path "publish")) {
+        New-Item -ItemType Directory -Path "publish"
+    }
     
     & npm pack --pack-destination "$PSScriptRoot/sdk/publish"
 }
