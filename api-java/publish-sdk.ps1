09753517c134d0968ecd9e6b4c721fe69e143f17
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-08-28 10:23:06 -0700

    Mmmh, interesting

--- api-java/publish-sdk.ps1
+++ api-java/publish-sdk.ps1
@@ -1,6 +1,5 @@
-$sourceLocation = "$PSScriptRoot/../dist/java"
-$destinationLocation = "$PSScriptRoot/../java"
+$destinationLocation = "$PSScriptRoot/sdk"
 $repositoryPath = "trinsic-id/sdk-java-api"
 $name = "Java"
 
-& "$PSScriptRoot\submodule.ps1" -tagPrefix '' -sourceLocation $sourceLocation -name $name -destinationLocation $destinationLocation -sdkRepositoryPath $Env:SDK_REPOSITORY_PATH -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
+& "$PSScriptRoot\submodule.ps1" -tagPrefix '' -name $name -destinationLocation $destinationLocation -sdkRepositoryPath $Env:SDK_REPOSITORY_PATH -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
--- api-php/publish-sdk.ps1
+++ api-php/publish-sdk.ps1
@@ -1,6 +1,5 @@
-$sourceLocation = "$PSScriptRoot/../dist/php"
-$destinationLocation = "$PSScriptRoot/../php"
+$destinationLocation = "$PSScriptRoot/sdk"
 $repositoryPath = "trinsic-id/sdk-php-api"
 $name = "PHP"
 
-& "$PSScriptRoot\submodule.ps1" -sourceLocation $sourceLocation -name $name -destinationLocation $destinationLocation -sdkRepositoryPath $Env:SDK_REPOSITORY_PATH -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
+& "$PSScriptRoot\submodule.ps1" -name $name -destinationLocation $destinationLocation -sdkRepositoryPath $Env:SDK_REPOSITORY_PATH -githubPAT $Env:PAT_GITHUB -repositoryPath $repositoryPath -packageVersion $Env:PACKAGE_VERSION
