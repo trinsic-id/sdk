685a6b76e66f129d8d16e61c85951350e176abad
Joshua Coffey <josh@trinsic.id>
2024-08-29 12:33:05 -0400

    Ensure flutter package version is in CHANGELOG.md

--- ui-flutter/publish-sdk.ps1
+++ ui-flutter/publish-sdk.ps1
@@ -14,6 +14,14 @@ try {
     $fileContent = $fileContent -replace "^version: [\d\.]+$", "version: $packageVersion"
     $fileContent | Set-Content -Path "pubspec.yaml"
 
+    Write-Host "Ensuring CHANGELOG.md starts with version $packageVersion"
+    $changelogContent = Get-Content -Path "CHANGELOG.md"
+    if ($changelogContent -notmatch "## $packageVersion") {
+        Write-Host "CHANGELOG.md needs patching -- doing so"
+        $changelogContent = "## $packageVersion`n`n- Auto-Generated changelog -- please visit https://github.com/trinsic-id/sdk for info`n`n$changelogContent"
+        $changelogContent | Set-Content -Path "CHANGELOG.md"
+    }
+
     Write-Host "Publishing package"
     if ($dryRun) {
         flutter pub publish --dry-run
