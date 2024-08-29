01ccecbd1da8a66ad205f927c24d2100bab456de
Joshua Coffey <josh@trinsic.id>
2024-08-29 12:59:36 -0400

    Flutter publish + sample improvements (#72)

--- ui-flutter/publish-sdk.ps1
+++ ui-flutter/publish-sdk.ps1
@@ -3,12 +3,24 @@ param(
 )
 
 try {
-    Set-Location "$PSScriptRoot/sdk"
-    # Write-Host "Setting version to $version"
+    Push-Location "$PSScriptRoot/sdk"
 
-    # $fileContent = Get-Content -Path "pubspe c.yaml"
-    # $fileContent = $fileContent -replace "VERSION_REPLACEME", "$version"
-    # $fileContent | Set-Content -Path "pubspec.yaml"
+    Write-Host "Getting current version"
+    $packageVersion = &"$PSScriptRoot\..\get-version.ps1" -versionName "flutterUIVersion"
+
+    Write-Host "Setting version to $packageVersion"
+
+    $fileContent = Get-Content -Path "pubspec.yaml"
+    $fileContent = $fileContent -replace "^version: [\d\.]+$", "version: $packageVersion"
+    $fileContent | Set-Content -Path "pubspec.yaml"
+
+    Write-Host "Ensuring CHANGELOG.md starts with version $packageVersion"
+    $changelogContent = Get-Content -Path "CHANGELOG.md"
+    if ($changelogContent -notmatch "## $packageVersion") {
+        Write-Host "CHANGELOG.md needs patching -- doing so"
+        $changelogContent = "## $packageVersion`n`n- Auto-Generated changelog -- please visit https://github.com/trinsic-id/sdk for info`n`n$changelogContent"
+        $changelogContent | Set-Content -Path "CHANGELOG.md"
+    }
 
     Write-Host "Publishing package"
     if ($dryRun) {
