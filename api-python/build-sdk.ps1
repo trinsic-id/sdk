643b701fd6f92c60acd3d74ed96a3fa90935d9b7
Lucas Castro <53352488+lucasamonrc@users.noreply.github.com>
2024-09-03 19:23:27 -0500

    Python and PHP package descriptions (#86)

--- api-python/build-sdk.ps1
+++ api-python/build-sdk.ps1
@@ -7,7 +7,42 @@ $additionalProperties = @{
 & "$PSScriptRoot/../helpers/generate-client.ps1" -language "python" -outputFolder "$PSScriptRoot/sdk" -additionalProperties $additionalProperties
 
 try {
+    Copy-Item "$PSScriptRoot/README.md" "$PSScriptRoot/sdk/README.md"
+
     Push-Location "$PSScriptRoot/sdk"
+
+    # Remove all content starting from the line that begins with 'setup(' in the setup.py file
+    $setupFilePath = "$PSScriptRoot/sdk/setup.py"
+    $content = Get-Content $setupFilePath
+    $index = $content.IndexOf($content -match '^\s*setup\(')  # Find the index of the line that starts with 'setup('
+    if ($index -ge 0) {
+        $newContent = $content[0..($index-1)]  # Keep everything before the 'setup(' line
+        $newContent | Set-Content $setupFilePath
+    }
+
+    # Append the contents of _setup.py to the modified setup.py
+    $additionalContent = @"
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Trinsic API",
+    author="trinsic-id",
+    author_email="support@trinsic.id",
+    url="https://trinsic.id",
+    keywords=["Trinsic", "SDK", "Identity verification"],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    package_data={"trinsic_api": ["py.typed"]},
+)
+"@
+    Add-Content -Path $setupFilePath -Value $additionalContent
+    
     # Check if 'python' is available
     $pythonPath = (Get-Command python -ErrorAction SilentlyContinue).Path
     $pipPath = (Get-Command pip -ErrorAction SilentlyContinue).Path
