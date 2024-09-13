2d4b1b725966d9a86dd1670eec9e12c2a6288ca0
Joshua Coffey <josh@trinsic.id>
2024-09-13 11:03:19 -0400

    Add Flutter testbed (#230)

---
+++ ui-flutter/sdk/testbed/android/app/src/main/java/id/trinsic/trinsic_flutter_ui_testbed/MainActivity.java
@@ -0,0 +1,6 @@
+package id.trinsic.trinsic_flutter_ui_testbed;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ui-flutter/sdk/testbed/lib/main.dart
@@ -0,0 +1,121 @@
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:trinsic_flutter_ui/trinsic_flutter_ui.dart';
+
+// Replace the below with a URL that, when called with a GET request, will return a session launch URL as the only text content of the response.
+// It will likely do so by using the Trinsic backend API SDK to create a session and return the launch URL.
+const String BACKEND_CREATE_SESSION_ENDPOINT = '';
+
+// Replace the below with a URL that uses a custom scheme that you've properly registered in your app's AndroidManifest.xml
+// The path (in this case "/callback") can be anything.
+const String CALLBACK_REDIRECT_URL =
+    "trinsic-flutter-ui-testbed-redirect-scheme:///callback";
+
+//----------- No need to modify anything below here -----------
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _stateText = 'Welcome!';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void setStateText(String text) {
+    setState(() {
+      _stateText = text;
+    });
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    try {
+      platformVersion = await TrinsicFlutter.getPlatformVersion() ??
+          'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setStateText("Running on: $platformVersion");
+  }
+
+  Future<String> getLaunchUrl() async {
+    setStateText("Getting launch URL...");
+    return await http.read(Uri.parse(BACKEND_CREATE_SESSION_ENDPOINT));
+  }
+
+  Future<void> launchSession() async {
+    var launchUrl = await getLaunchUrl();
+
+    setStateText("Launching...");
+    Map? result;
+    try {
+      result =
+          await TrinsicFlutter.launchSession(launchUrl, CALLBACK_REDIRECT_URL);
+    } on PlatformException {
+      setStateText("Failed to launch");
+      result = null;
+      return;
+    }
+
+    if (result == null) {
+      setStateText("Result null");
+      return;
+    }
+
+    if (result.containsKey("resultsAccessKey") &&
+        result["resultsAccessKey"] != null &&
+        result["resultsAccessKey"]!.toString().isNotEmpty) {
+      setStateText("Got ResultAccessKey: ${result!["resultsAccessKey"]}");
+      return;
+    }
+
+    if (result.containsKey("canceled") && result["canceled"] == true) {
+      setStateText("User canceled");
+      return;
+    }
+
+    if (result.containsKey("success") && result["success"] == false) {
+      setStateText("Session failed");
+      return;
+    }
+
+    setStateText("Unknown result");
+    return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Trinsic Flutter UI Testbed'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: launchSession,
+            tooltip: 'Launch Session',
+            child: const Icon(Icons.add)),
+        body: Center(
+          child: Text(_stateText),
+        ),
+      ),
+    );
+  }
+}
