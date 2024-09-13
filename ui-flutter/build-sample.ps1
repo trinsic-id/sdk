db8649319c0f2f1f70fbef39afb485230eac6334
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-09-13 10:27:12 -0700

    Build Flutter iOS sample

--- ui-flutter/build-sample.ps1
+++ ui-flutter/build-sample.ps1
@@ -4,16 +4,25 @@ try {
     Write-Host "Building ui-flitter sample project..."
     Push-Location $FLUTTER_SAMPLES_DIR
 
-    flutter build apk
-    $exitCode = $LASTEXITCODE
 
-    Pop-Location
+    Write-Host "Building Flutter iOS..."
+    &flutter build ios
 
-    if ($exitCode -ne 0) {
-        throw "The flutter build APK command failed with exit code $exitCode"
-    }
+    if ($LASTEXITCODE -ne 0) {
+        throw "The flutter build ios command failed with exit code $LASTEXITCODE"
+    }    
 
-} catch {
+    Write-Host "Building Flutter Android APK..."
+    &flutter build apk
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "The flutter build APK command failed with exit code $LASTEXITCODE"
+    }    
+}
+catch {
     Write-Host "An error occurred: $_" -ForegroundColor Red
     Exit 1
+}
+finally {
+    Pop-Location
 }
