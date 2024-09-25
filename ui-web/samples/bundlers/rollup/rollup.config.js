43441fd55e95b4693d88e0e94b00bd2732c84475
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2024-09-25 12:44:51 -0700

    Finish rollup

---
+++ ui-web/samples/bundlers/rollup/rollup.config.js
@@ -0,0 +1,9 @@
+// rollup.config.js
+export default {
+  input: "src/main.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife", // Suitable for <script> tags in browsers
+    name: "MyBundle", // Global variable name
+  },
+};
