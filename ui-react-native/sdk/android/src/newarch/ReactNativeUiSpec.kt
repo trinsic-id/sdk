6445adeb3f5f0638eb32e6ce169c3c3ea5efb716
trinsic-josh <josh+github@trinsic.id>
2025-04-10 11:31:03 -0400

    Remove `ui-react-native` SDK (#331)

--- ui-react-native/sdk/android/src/newarch/ReactNativeUiSpec.kt
+++
@@ -1,7 +0,0 @@
-package com.trinsic.reactnativeui
-
-import com.facebook.react.bridge.ReactApplicationContext
-
-abstract class ReactNativeUiSpec internal constructor(context: ReactApplicationContext) :
-  NativeReactNativeUiSpec(context) {
-}
