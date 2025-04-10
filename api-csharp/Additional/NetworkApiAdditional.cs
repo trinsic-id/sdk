795eecc8599a873c9afcca66cf3babee9063688f
Joshua Coffey <josh+github@trinsic.id>
2025-04-10 17:36:31 -0400

    C#: API Token improvements

---
+++ api-csharp/Additional/NetworkApiAdditional.cs
@@ -0,0 +1,25 @@
+using System;
+using Trinsic.Api.Client;
+
+namespace Trinsic.Api.Api;
+
+public partial interface INetworkApi
+{
+    /// <summary>
+    /// Sets the auth token used for all requests made by this client.
+    /// </summary>
+    public void SetAuthToken(string token);
+}
+
+public partial class NetworkApi
+{
+    public void SetAuthToken(string token)
+    {
+        if (BearerTokenProvider is not ConstantTokenProvider<BearerToken> constantProvider)
+        {
+            throw new Exception("SetAuthToken() cannot be called if you are not using the ConstantTokenProvider.");
+        }
+
+        constantProvider._token = new BearerToken(token);
+    }
+}
