9d96e06e27d27093a03800048b434b47f782053b
Lucas Castro <lucasamonrc@gmail.com>
2025-05-16 12:59:30 -0500

    Add start script to the php project

---
+++ api-php/samples/server/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -f .env ] || ! grep -q "TRINSIC_ACCESS_TOKEN" .env; then
+    echo "Error: .env file not found or TRINSIC_ACCESS_TOKEN not set in .env file"
+    exit 1
+fi
+
+echo "Build UI sample..."
+cd ../../../ui-web/samples
+npm install
+npm run build
+
+echo "Preparing PHP API sample..."
+cd ../../api-php/samples/server
+composer install
+
+# Fail if the composer install fails
+if [ $? -ne 0 ]; then
+    echo "Error: composer install failed"
+    exit 1
+fi
+
+echo "Starting PHP API sample..."
+php -S localhost:3000 -t public
