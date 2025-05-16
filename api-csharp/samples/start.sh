72982adeb018225f37bea64b913b1e68c7eaed31
Lucas Castro <lucasamonrc@gmail.com>
2025-05-16 11:47:52 -0500

    Add start script to the c# project

---
+++ api-csharp/samples/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$TRINSIC_ACCESS_TOKEN" ]; then
+    if [ ! -f .env ] || ! grep -q "TRINSIC_ACCESS_TOKEN" .env; then
+        echo "Error: TRINSIC_ACCESS_TOKEN environment variable not set or .env file not found"
+        exit 1
+    fi
+fi
+
+echo "Build UI sample..."
+cd ../../ui-web/samples
+npm install
+npm run build
+
+echo "Starting C# API sample..."
+cd ../../api-csharp/samples
+dotnet run --project ./Sample/Sample.csproj
