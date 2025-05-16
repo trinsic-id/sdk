114c88ccf9c5e09a25266deb493be2dae55a561d
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2025-05-16 14:32:13 -0700

    Fine tune python start script

--- api-python/samples/server/start.sh
+++ api-python/samples/server/start.sh
@@ -12,14 +12,43 @@ cd ../../../ui-web/samples
 npm ci
 npm run build
 
-echo "Installing Python dependencies..."
+
 cd ../../api-python/samples/server
-pip3 install -r requirements.txt --break-system-packages --user
+
+# Check if 'python' is available
+pythonPath=$(command -v python 2>/dev/null)
+pipPath=$(command -v pip 2>/dev/null)
+
+# If 'python' is not available, check for 'python3'
+if [ -z "$pythonPath" ]; then
+    pythonPath=$(command -v python3 2>/dev/null)
+    pipPath=$(command -v pip3 2>/dev/null)
+fi
+
+# If neither 'python' nor 'python3' is available, print an error and exit
+if [ -z "$pythonPath" ]; then
+    echo "Neither 'python' nor 'python3' was found in your system path." >&2
+    exit 1
+fi
+
+echo "Setting up Python virtual environment..."
+"$pythonPath" -m venv venv
+if [ $? -ne 0 ]; then
+    echo "Failed to create virtual environment." >&2
+    exit 1
+fi
+
+echo "Activating virtual environment..."
+# Activate the virtual environment
+. ./venv/bin/activate
+
+echo "Installing Python dependencies..."
+pip install -r requirements.txt 
 
 if [ $? -ne 0 ]; then
-    echo "Error: pip3 install failed"
+    echo "Error: Failed to install dependencies"
     exit 1
 fi
 
 echo "Starting Python API sample..."
-python3 main.py
+"$pythonPath" main.py
