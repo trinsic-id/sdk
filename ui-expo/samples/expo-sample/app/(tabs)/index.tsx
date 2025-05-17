3416d47c387ae8f01bb6d51dd20a867555ac7016
Jan-Pieter George <jp@trinsic.id>
2025-05-16 18:10:53 -0700

    Update expo sample with latest SDK update (#356)

--- ui-expo/samples/expo-sample/app/(tabs)/index.tsx
+++ ui-expo/samples/expo-sample/app/(tabs)/index.tsx
@@ -4,17 +4,21 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { useState } from "react";
+import * as Linking from "expo-linking";
 import { launchSession, LaunchSessionResult } from "@trinsic/expo-ui";
 const SESSION_CREATE_URL = "https://api.trinsic.id/api/mobiletest/create-session";
 
 export default function HomeScreen() {
   const [result, setResult] = useState<LaunchSessionResult | null>();
   const handleButtonPress = async () => {
-    const getSessionUrlResult = await fetch(SESSION_CREATE_URL);
+    const callbackPath = "/";
+    const url = Linking.createURL(callbackPath);
+    const fetchUrl = SESSION_CREATE_URL + "?redirectUrl=" +encodeURIComponent(url);
+    console.log("Creating session at fake API endpoint: ", fetchUrl);
+    const getSessionUrlResult = await fetch(fetchUrl);
     const launchUrl = await getSessionUrlResult.text();
-    const result = await launchSession(launchUrl, {
-      callbackPath: "/",
-    });
+    console.log("Launch URL: ", launchUrl);
+    const result = await launchSession(launchUrl, callbackPath);
     setResult(result);
   };
   return (
