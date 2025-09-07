29e8d496b5969c91c6c1e4ae0c9d889a0998ba97
Joshua Coffey <josh+github@trinsic.id>
2025-09-07 16:55:28 -0400

    Update Android Sample for mDL Exchange API

--- ui-android/samples/android_sample/app/build.gradle.kts
+++ ui-android/samples/android_sample/app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
         applicationId = "id.trinsic.android_sample"
         minSdk = 26
         targetSdk = 34
+        compileSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,5 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.trinsic-id:sdk-android-ui:2.0.0")
+    implementation("com.github.trinsic-id:sdk-android-ui:2.1.0-alpha1")
 }
