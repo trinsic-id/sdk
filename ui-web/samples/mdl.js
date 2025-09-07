f378bb85609cab64eee4139db5a4cc8940b010db
Joshua Coffey <josh+github@trinsic.id>
2025-09-07 17:25:00 -0400

    Work on mDL sample

---
+++ ui-web/samples/mdl.js
@@ -0,0 +1,27 @@
+import { launchRedirect, launchPopup } from "@trinsic/web-ui";
+import MicroModal from "micromodal";
+MicroModal.init();
+
+window.launchHostedProvider = launchHostedProvider;
+
+async function launchHostedProvider(providerId) {
+  const launchUrl = `hosted-launch/${providerId}?1=1`
+  let launchMode = document.querySelector('input[name="hostedLaunch"]:checked').value;
+  await launch(launchUrl, launchMode)
+}
+
+async function launch(launchUrl, launchMode) {
+  let result = null;
+  switch (launchMode) {
+    case 'popup':
+      result = await launchPopup(() => launchUrl + '&redirectUrl=' + window.location.origin + '/redirect');
+      await exchangeResult(result);
+      break;
+    case 'redirect':
+      //Result is exchanged through the redirect capture, see redirect.html
+      await launchRedirect(launchUrl + '&redirectUrl='+window.location.origin + '/redirect');
+      break;
+  }
+}
+
+getProviders('launchHostedProvider');
