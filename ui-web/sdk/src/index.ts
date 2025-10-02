1abee23f4f407189cefea35d51e03755e13c3829
Joshua Coffey <josh+github@trinsic.id>
2025-10-02 17:47:22 -0400

    Export `TrinsicPopupResult`

--- ui-web/sdk/src/index.ts
+++ ui-web/sdk/src/index.ts
@@ -1,9 +1,9 @@
 import { launchRedirect } from "./launchRedirect";
 import { createPopup, createPopupAndWaitForResults, CreatePopupAndWaitForResultsOptions } from "./launchPopup";
-import { TrinsicPopup, WaitForResultsOptions, TrinsicPopupResultCode } from "./TrinsicPopup";
+import { TrinsicPopup, TrinsicPopupResult, WaitForResultsOptions, TrinsicPopupResultCode } from "./TrinsicPopup";
 import { MdlExchangeResult, performMdlExchange, isUserOnEligibleAppleWalletBrowser, isUserOnEligibleGoogleWalletBrowser } from "./mdlExchange";
 import { signalRedirectFromPopup, SignalRedirectFromPopupOptions } from "./signalRedirectFromPopup";
 
 
 export { launchRedirect, signalRedirectFromPopup, createPopup, createPopupAndWaitForResults, TrinsicPopup, TrinsicPopupResultCode, performMdlExchange, isUserOnEligibleAppleWalletBrowser, isUserOnEligibleGoogleWalletBrowser };
-export type { SignalRedirectFromPopupOptions, CreatePopupAndWaitForResultsOptions, WaitForResultsOptions, MdlExchangeResult };
+export type { TrinsicPopupResult, SignalRedirectFromPopupOptions, CreatePopupAndWaitForResultsOptions, WaitForResultsOptions, MdlExchangeResult };
