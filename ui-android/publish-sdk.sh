9d9b681e874b01242a19dad365c3e05897fb2003
Joshua Coffey <josh+github@trinsic.id>
2026-05-05 14:45:35 -0400

    Update deploy scripts

--- ui-android/publish-sdk.sh
+++ ui-android/publish-sdk.sh
@@ -8,10 +8,13 @@ source "$REPO_ROOT/helpers/common.sh"
 
 require_env SDK_REPOSITORY_PATH
 require_env MEWMBA_GIT_PUBLISH_TOKEN
+require_env ORG_GRADLE_PROJECT_mavenCentralUsername
+require_env ORG_GRADLE_PROJECT_mavenCentralPassword
+require_env ORG_GRADLE_PROJECT_signingInMemoryKey
+require_env ORG_GRADLE_PROJECT_signingInMemoryKeyPassword
 
+# Commit updated build.gradle.kts, tag the commit, and push
 (
-  cd "$SCRIPT_DIR/sdk"
-
   git config --global user.name "github-actions[bot]"
   git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
@@ -34,11 +37,13 @@ require_env MEWMBA_GIT_PUBLISH_TOKEN
   echo "Pushing to submodule repository"
   git push origin main
 
-  tag_name="$package_version"
-  git tag "$tag_name"
+  # Don't tag so we don't trigger Jitpack (testing)
+  # tag_name="$package_version"
+  # git tag "$tag_name"
   git push origin "$tag_name"
 )
 
+# Push updated submodule ref to root repo
 (
   cd "$REPO_ROOT"
   remote_origin="https://$MEWMBA_GIT_PUBLISH_TOKEN@github.com/$SDK_REPOSITORY_PATH.git"
@@ -48,4 +53,10 @@ require_env MEWMBA_GIT_PUBLISH_TOKEN
   git add "ui-android/sdk"
   git commit -m "Update ui-android submodule reference to version $(get_version androidUIVersion)"
   git push origin main
+)
+
+# Publish to Maven
+(
+  cd "$_SCRIPT_DIR/sdk"
+  ./gradlew publishToMavenCentral
 )
